808361114367344557b8c08ca2a855c9e0443a49
Guintu <andreguintu26@gmail.com>
2024-04-08 10:19:40 +0800

    Add project files.

---
+++ atm2/Form2.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace atm2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            guna2Shapes1 = new Guna.UI2.WinForms.Guna2Shapes();
+            btnbal = new Guna.UI2.WinForms.Guna2GradientButton();
+            guna2GradientButton1 = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnDeposit = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnWithdraw = new Guna.UI2.WinForms.Guna2GradientButton();
+            label5 = new Label();
+            guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            guna2GradientPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.BackColor = Color.Transparent;
+            guna2GradientPanel1.Controls.Add(guna2Shapes1);
+            guna2GradientPanel1.Controls.Add(btnbal);
+            guna2GradientPanel1.Controls.Add(guna2GradientButton1);
+            guna2GradientPanel1.Controls.Add(btnDeposit);
+            guna2GradientPanel1.Controls.Add(btnWithdraw);
+            guna2GradientPanel1.Controls.Add(label5);
+            guna2GradientPanel1.Controls.Add(guna2Button5);
+            guna2GradientPanel1.CustomizableEdges = customizableEdges25;
+            guna2GradientPanel1.FillColor = Color.FromArgb(46, 86, 118);
+            guna2GradientPanel1.FillColor2 = Color.FromArgb(77, 67, 118);
+            guna2GradientPanel1.ForeColor = Color.Transparent;
+            guna2GradientPanel1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            guna2GradientPanel1.Location = new Point(-4, -1);
+            guna2GradientPanel1.Margin = new Padding(3, 2, 3, 2);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges26;
+            guna2GradientPanel1.Size = new Size(729, 315);
+            guna2GradientPanel1.TabIndex = 2;
+            // 
+            // guna2Shapes1
+            // 
+            guna2Shapes1.BackColor = Color.Transparent;
+            guna2Shapes1.BorderColor = Color.Transparent;
+            guna2Shapes1.FillColor = Color.White;
+            guna2Shapes1.ForeColor = Color.Transparent;
+            guna2Shapes1.LineThickness = 2;
+            guna2Shapes1.Location = new Point(-30, 37);
+            guna2Shapes1.Name = "guna2Shapes1";
+            guna2Shapes1.PolygonSkip = 1;
+            guna2Shapes1.Rotate = 0F;
+            guna2Shapes1.RoundedEdges = customizableEdges14;
+            guna2Shapes1.Shape = Guna.UI2.WinForms.Enums.ShapeType.Line;
+            guna2Shapes1.Size = new Size(786, 13);
+            guna2Shapes1.TabIndex = 36;
+            guna2Shapes1.Text = "guna2Shapes1";
+            guna2Shapes1.Zoom = 80;
+            // 
+            // btnbal
+            // 
+            btnbal.BorderRadius = 20;
+            btnbal.CustomizableEdges = customizableEdges15;
+            btnbal.DisabledState.BorderColor = Color.DarkGray;
+            btnbal.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnbal.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnbal.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnbal.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnbal.FillColor = Color.FromArgb(66, 208, 116);
+            btnbal.FillColor2 = Color.FromArgb(128, 255, 128);
+            btnbal.Font = new Font("Poppins", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnbal.ForeColor = Color.MidnightBlue;
+            btnbal.Image = (Image)resources.GetObject("btnbal.Image");
+            btnbal.ImageOffset = new Point(6, 0);
+            btnbal.ImageSize = new Size(60, 60);
+            btnbal.Location = new Point(376, 157);
+            btnbal.Name = "btnbal";
+            btnbal.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            btnbal.Size = new Size(272, 94);
+            btnbal.TabIndex = 35;
+            btnbal.Text = "Check Balance";
+            btnbal.TextOffset = new Point(-3, 3);
+            btnbal.Click += guna2GradientButton2_Click;
+            // 
+            // guna2GradientButton1
+            // 
+            guna2GradientButton1.BorderRadius = 20;
+            guna2GradientButton1.CustomizableEdges = customizableEdges17;
+            guna2GradientButton1.DisabledState.BorderColor = Color.DarkGray;
+            guna2GradientButton1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2GradientButton1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2GradientButton1.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            guna2GradientButton1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2GradientButton1.FillColor = Color.FromArgb(239, 123, 160);
+            guna2GradientButton1.FillColor2 = Color.FromArgb(239, 139, 106);
+            guna2GradientButton1.Font = new Font("Poppins", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guna2GradientButton1.ForeColor = Color.MidnightBlue;
+            guna2GradientButton1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            guna2GradientButton1.Image = (Image)resources.GetObject("guna2GradientButton1.Image");
+            guna2GradientButton1.ImageSize = new Size(60, 60);
+            guna2GradientButton1.Location = new Point(376, 56);
+            guna2GradientButton1.Name = "guna2GradientButton1";
+            guna2GradientButton1.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            guna2GradientButton1.Size = new Size(272, 94);
+            guna2GradientButton1.TabIndex = 34;
+            guna2GradientButton1.Text = "Pay Bills";
+            // 
+            // btnDeposit
+            // 
+            btnDeposit.BorderRadius = 20;
+            btnDeposit.CustomizableEdges = customizableEdges19;
+            btnDeposit.DisabledState.BorderColor = Color.DarkGray;
+            btnDeposit.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnDeposit.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnDeposit.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnDeposit.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnDeposit.FillColor = Color.FromArgb(255, 255, 128);
+            btnDeposit.FillColor2 = Color.FromArgb(239, 139, 106);
+            btnDeposit.Font = new Font("Poppins", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDeposit.ForeColor = Color.MidnightBlue;
+            btnDeposit.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            btnDeposit.Image = (Image)resources.GetObject("btnDeposit.Image");
+            btnDeposit.ImageOffset = new Point(-6, 0);
+            btnDeposit.ImageSize = new Size(60, 60);
+            btnDeposit.Location = new Point(98, 156);
+            btnDeposit.Name = "btnDeposit";
+            btnDeposit.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            btnDeposit.Size = new Size(272, 94);
+            btnDeposit.TabIndex = 33;
+            btnDeposit.Text = "Deposit";
+            btnDeposit.TextOffset = new Point(-3, 3);
+            btnDeposit.Click += btnbal_Click;
+            // 
+            // btnWithdraw
+            // 
+            btnWithdraw.BorderRadius = 20;
+            btnWithdraw.CustomizableEdges = customizableEdges21;
+            btnWithdraw.DisabledState.BorderColor = Color.DarkGray;
+            btnWithdraw.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnWithdraw.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnWithdraw.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnWithdraw.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnWithdraw.FillColor = Color.FromArgb(8, 198, 173);
+            btnWithdraw.FillColor2 = Color.FromArgb(137, 218, 214);
+            btnWithdraw.Font = new Font("Poppins", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnWithdraw.ForeColor = Color.MidnightBlue;
+            btnWithdraw.Image = Properties.Resources.money_withdrawal;
+            btnWithdraw.ImageSize = new Size(60, 60);
+            btnWithdraw.Location = new Point(98, 56);
+            btnWithdraw.Name = "btnWithdraw";
+            btnWithdraw.ShadowDecoration.CustomizableEdges = customizableEdges22;
+            btnWithdraw.Size = new Size(272, 94);
+            btnWithdraw.TabIndex = 32;
+            btnWithdraw.Text = "Withdraw";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Verdana", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(43, 13);
+            label5.Name = "label5";
+            label5.Size = new Size(124, 23);
+            label5.TabIndex = 31;
+            label5.Text = "OhMyBank";
+            // 
+            // guna2Button5
+            // 
+            guna2Button5.BorderRadius = 20;
+            guna2Button5.CustomizableEdges = customizableEdges23;
+            guna2Button5.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button5.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button5.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button5.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button5.FillColor = Color.Brown;
+            guna2Button5.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2Button5.ForeColor = Color.White;
+            guna2Button5.Location = new Point(278, 260);
+            guna2Button5.Margin = new Padding(3, 2, 3, 2);
+            guna2Button5.Name = "guna2Button5";
+            guna2Button5.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            guna2Button5.Size = new Size(186, 38);
+            guna2Button5.TabIndex = 6;
+            guna2Button5.Text = "Back to Main";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(720, 308);
+            Controls.Add(guna2GradientPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            guna2GradientPanel1.ResumeLayout(false);
+            guna2GradientPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Label label5;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
+        private Guna.UI2.WinForms.Guna2GradientButton btnWithdraw;
+        private Guna.UI2.WinForms.Guna2GradientButton btnDeposit;
+        private Guna.UI2.WinForms.Guna2GradientButton btnbal;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton1;
+        private Guna.UI2.WinForms.Guna2Shapes guna2Shapes1;
+    }
+}
---
+++ atm2/checkbalance.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace atm2
+{
+    partial class checkbalance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            txtboxBal = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            label1 = new Label();
+            guna2Button9 = new Guna.UI2.WinForms.Guna2Button();
+            label5 = new Label();
+            guna2GradientPanel1.SuspendLayout();
+            guna2CustomGradientPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.BackColor = Color.Transparent;
+            guna2GradientPanel1.Controls.Add(guna2CustomGradientPanel1);
+            guna2GradientPanel1.Controls.Add(guna2Button9);
+            guna2GradientPanel1.Controls.Add(label5);
+            guna2GradientPanel1.CustomizableEdges = customizableEdges5;
+            guna2GradientPanel1.FillColor = Color.FromArgb(46, 86, 118);
+            guna2GradientPanel1.FillColor2 = Color.FromArgb(77, 67, 118);
+            guna2GradientPanel1.ForeColor = Color.Transparent;
+            guna2GradientPanel1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            guna2GradientPanel1.Location = new Point(-7, 0);
+            guna2GradientPanel1.Margin = new Padding(3, 2, 3, 2);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2GradientPanel1.Size = new Size(744, 318);
+            guna2GradientPanel1.TabIndex = 5;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            guna2CustomGradientPanel1.BackColor = Color.Transparent;
+            guna2CustomGradientPanel1.BorderRadius = 16;
+            guna2CustomGradientPanel1.Controls.Add(txtboxBal);
+            guna2CustomGradientPanel1.Controls.Add(label1);
+            guna2CustomGradientPanel1.CustomizableEdges = customizableEdges1;
+            guna2CustomGradientPanel1.FillColor = Color.FromArgb(128, 255, 128);
+            guna2CustomGradientPanel1.FillColor2 = Color.FromArgb(66, 208, 116);
+            guna2CustomGradientPanel1.FillColor3 = Color.FromArgb(128, 255, 128);
+            guna2CustomGradientPanel1.FillColor4 = Color.FromArgb(66, 208, 116);
+            guna2CustomGradientPanel1.Location = new Point(89, 98);
+            guna2CustomGradientPanel1.Margin = new Padding(3, 2, 3, 2);
+            guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            guna2CustomGradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2CustomGradientPanel1.Size = new Size(554, 132);
+            guna2CustomGradientPanel1.TabIndex = 62;
+            // 
+            // txtboxBal
+            // 
+            txtboxBal.BackColor = Color.Transparent;
+            txtboxBal.Font = new Font("Poppins", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtboxBal.ForeColor = Color.MidnightBlue;
+            txtboxBal.Location = new Point(230, 57);
+            txtboxBal.Name = "txtboxBal";
+            txtboxBal.Size = new Size(87, 44);
+            txtboxBal.TabIndex = 62;
+            txtboxBal.Text = "------";
+            txtboxBal.TextAlignment = ContentAlignment.MiddleCenter;
+            txtboxBal.Click += guna2HtmlLabel1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Poppins", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.MidnightBlue;
+            label1.Location = new Point(176, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 37);
+            label1.TabIndex = 59;
+            label1.Text = "Account Balance";
+            // 
+            // guna2Button9
+            // 
+            guna2Button9.BorderRadius = 20;
+            guna2Button9.CustomizableEdges = customizableEdges3;
+            guna2Button9.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button9.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button9.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button9.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button9.FillColor = Color.FromArgb(255, 128, 128);
+            guna2Button9.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2Button9.ForeColor = Color.Black;
+            guna2Button9.Location = new Point(509, 244);
+            guna2Button9.Margin = new Padding(3, 2, 3, 2);
+            guna2Button9.Name = "guna2Button9";
+            guna2Button9.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button9.Size = new Size(134, 32);
+            guna2Button9.TabIndex = 60;
+            guna2Button9.Text = "Back";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(45, 27);
+            label5.Name = "label5";
+            label5.Size = new Size(145, 26);
+            label5.TabIndex = 58;
+            label5.Text = "OhMyBank";
+            // 
+            // checkbalance
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(730, 311);
+            Controls.Add(guna2GradientPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "checkbalance";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "checkbalance";
+            guna2GradientPanel1.ResumeLayout(false);
+            guna2GradientPanel1.PerformLayout();
+            guna2CustomGradientPanel1.ResumeLayout(false);
+            guna2CustomGradientPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Label label1;
+        private Label label5;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button9;
+        private Guna.UI2.WinForms.Guna2HtmlLabel txtboxBal;
+    }
+}
